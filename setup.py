29be4dc05d419b465984d163501eac2082f32cc4
Adam <sk82712@gmail.com>
2015-05-30 13:28:48 -0400

    0.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='Flask-Material',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/HellerCommaA/flask-materialize',
     license='MIT',
     author='Adam Heller',
